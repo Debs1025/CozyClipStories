6ececb13865a23904cb2028ad7162aab7d4b9a74
Erick De Belen <erickdebelen22@gmail.com>
2025-10-27 23:04:29 +0800

    Set JSON limit to 5mb in express middleware
    Increase JSON payload limit to 5mb.

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(userRoute);
 app.use(studentRoute);
 app.use(teacherRoute);
